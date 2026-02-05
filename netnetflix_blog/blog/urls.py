8e0abc816925d7a47765da19b82591c0ea111374
valeriochristiandevera <valeriochristiannn@gmail.com>
2026-02-05 12:00:21 +0800

    Reorder post_detail path in urls.py

--- netnetflix_blog/blog/urls.py
+++ netnetflix_blog/blog/urls.py
@@ -5,8 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('post/<slug:slug>/', views.post_detail_view, name='post_detail'),
     path('post/create/', views.post_create_view, name='post_create'),
+    path('post/<slug:slug>/', views.post_detail_view, name='post_detail'),
     path('post/<slug:slug>/edit/', views.post_edit_view, name='post_edit'),
     path('post/<slug:slug>/delete/', views.post_delete_view, name='post_delete'),
 ]
